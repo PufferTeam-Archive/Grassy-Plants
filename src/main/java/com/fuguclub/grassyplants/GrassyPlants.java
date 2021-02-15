097c9fdd7a326869af1fc05abf068c56be73b6a9
minemobs <issahakim94@gmail.com>
2021-02-15 21:24:19 +0100

    Changed some usless things

--- src/main/java/com/fuguclub/grassyplants/GrassyPlants.java
+++ src/main/java/com/fuguclub/grassyplants/GrassyPlants.java
@@ -9,9 +9,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("grassyplants")
-
+@Mod.EventBusSubscriber(modid = "grassyplants",bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GrassyPlants {
     public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "grassyplants";
 
     public GrassyPlants() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
